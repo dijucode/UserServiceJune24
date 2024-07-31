08ed7fa4ccf2888b6ce9a9f443a5fb488ba0c525
dijucode <itsdiju@gmail.com>
2024-07-31 23:04:39 +0530

    added springboot actuator health

--- src/test/java/com/objectville/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/objectville/userservice/UserServiceApplicationTests.java
@@ -15,32 +15,32 @@ import java.util.UUID;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-    @Autowired
-    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
-
-    @Test
-    void contextLoads() {
-    }
-
+//    @Autowired
+//    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
 //    @Test
 //    public void addSampleRegisteredClient() {
-//        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-//                .clientId("oidc-client")
-//                .clientSecret("$2a$12$qMORUnSc.wq14q28m.e3/.GYRmgQdWJCPTrX2QGZbI3KChYZ1AJWO")
-//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-//                .redirectUri("https://oauth.pstmn.io/v1/callback")
-//                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
-//                .scope(OidcScopes.OPENID)
-//                .scope(OidcScopes.PROFILE)
-//                .scope("ADMIN")
-//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-//                .build();
-//
-//        jpaRegisteredClientRepository.save(oidcClient);
-//
-//
-//    }
+////        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+////                .clientId("oidc-client")
+////                .clientSecret("$2a$12$qMORUnSc.wq14q28m.e3/.GYRmgQdWJCPTrX2QGZbI3KChYZ1AJWO")
+////                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+////                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+////                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+////                .redirectUri("https://oauth.pstmn.io/v1/callback")
+////                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+////                .scope(OidcScopes.OPENID)
+////                .scope(OidcScopes.PROFILE)
+////                .scope("ADMIN")
+////                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+////                .build();
+////
+////        jpaRegisteredClientRepository.save(oidcClient);
+////
+////
+////    }
 
 }
